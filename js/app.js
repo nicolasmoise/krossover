385ee80fbdbda1fe5e724a18d1548112a8ff86db
Nicolas Moise <nicolas.moise@newscred.com>
2016-02-22 23:49:06 -0500

    General UI improvements
    - form validation if end is before start
    - CSS styling and general UI

--- js/app.js
+++ js/app.js
@@ -4,29 +4,38 @@ angular.module('krossoverExercize', [])
     $scope.clips = [];
 
     $scope.createClip = function () {
-      $scope.newClip = {};
+      $scope.clip = {};
       $scope.creatingClip = true;
     }
 
     $scope.selectFullClip = function () {
+      _unselectAllClips();
+      $scope.fullClipSelected = true;
       $scope.videoSource = $sce.trustAsResourceUrl('sintel_trailer-480.mp4');
     };
 
     $scope.select = function (clip) {
-        $scope.videoSource = $sce.trustAsResourceUrl('sintel_trailer-480.mp4#t='+ clip.start + ',' + clip.end);
+      _unselectAllClips();
+      clip.selected = true;
+      $scope.videoSource = $sce.trustAsResourceUrl('sintel_trailer-480.mp4#t='+ clip.start + ',' + clip.end);
     };
+
+    function _unselectAllClips () {
+      $scope.fullClipSelected = false;
+      $scope.clips= $scope.clips.map(function(clip) {
+        clip.selected = false;
+        return clip;
+      });
+    }
 
     $scope.addClip = function () {
-      $scope.clips.push($scope.newClip);
+      $scope.clips.push($scope.clip);
       $scope.creatingClip = false;
-    };
-
-    $scope.edit = function (clip) {
-      clip.editing = true;
     };
 
     $scope.save = function (clip) {
       clip.editing = false;
+      clip.showControls = false;
     };
 
     $scope.remove = function (index) {
