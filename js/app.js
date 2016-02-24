a4c60778ca0c07fa5f6d6e4013cf1c466f27660d
Nicolas Moise <nicolas.moise@newscred.com>
2016-02-24 14:30:25 -0500

    Hotkey support

--- js/app.js
+++ js/app.js
@@ -1,5 +1,9 @@
 angular.module('krossoverExercize', [])
-  .controller('clipPlayerController', ['$sce', '$scope', function($sce, $scope) {
+  .controller('clipPlayerController', ['$sce', '$document', '$timeout', '$scope', function ($sce, $document, $timeout, $scope) {
+
+    var VIDEO_URL = 'sintel_trailer-480.mp4';
+
+    $scope.videoPlayerView = 'noClipSelected';
 
     $scope.clips = [];
 
@@ -9,15 +13,17 @@ angular.module('krossoverExercize', [])
     };
 
     $scope.selectFullClip = function () {
+      $scope.videoPlayerView = 'clipPlaying';
       $scope.fullClipSelected = true;
       $scope.currentClip = null;
-      $scope.videoSource = $sce.trustAsResourceUrl('sintel_trailer-480.mp4');
+      $scope.videoSource = $sce.trustAsResourceUrl(VIDEO_URL);
     };
 
     $scope.select = function (clip) {
+      $scope.videoPlayerView = 'clipPlaying';
       $scope.fullClipSelected = false;
       $scope.currentClip = clip;
-      $scope.videoSource = $sce.trustAsResourceUrl('sintel_trailer-480.mp4#t='+ clip.start + ',' + clip.end);
+      $scope.videoSource = $sce.trustAsResourceUrl(VIDEO_URL + '#t='+ clip.start + ',' + clip.end);
     };
 
     $scope.addClip = function () {
@@ -33,6 +39,90 @@ angular.module('krossoverExercize', [])
     $scope.remove = function (index) {
       $scope.clips.splice(index, 1);
     };
+
+    $scope.playNextClip = function () {
+      $scope.videoPlayerView = 'clipLoading';
+      $timeout(function () {
+        if ($scope.videoSource === null && !$scope.showNextClipButton && !$scope.clipLoading) {
+          $scope.selectFullClip();
+        } else {
+          var indexOfNextClip = $scope.clips.indexOf($scope.currentClip) + 1;
+          var nextClip = $scope.clips[indexOfNextClip];
+          $scope.select(nextClip);
+        }
+      }, 3000);
+    };
+
+    $scope.playPreviousClip = function () {
+      $scope.videoPlayerView = 'clipLoading';
+      $timeout(function () {
+        var indexOfNextClip = $scope.clips.indexOf($scope.currentClip) + -1;
+        if(indexOfNextClip === -1) {
+          $scope.selectFullClip();
+        } else {
+          var nextClip = $scope.clips[indexOfNextClip];
+          $scope.select(nextClip);
+        }
+      }, 3000);
+    };
+
+    var videoplayer = angular.element($document[0].getElementById('video-player'));
+
+    videoplayer.on('timeupdate', function () {
+      if (currentClipIsOver() && thereIsANextClip()) {
+        $scope.videoPlayerView = 'clipFinished';
+        $scope.showNextClipButton = true;
+        $scope.$apply();
+      }
+    });
+
+    videoplayer.on('ended', function () {
+      $scope.videoPlayerView = 'clipFinished';
+      if (thereIsANextClip()) {
+        $scope.showNextClipButton = true;
+        $scope.$apply();
+      }
+    });
+
+    $document.on('keypress', function(e){
+       if (e.which === 113 && thereIsAPreviousCLip()){
+         videoplayer[0].pause();
+         $scope.videoPlayerView = 'clipLoading';
+         $scope.$apply();
+         $scope.playPreviousClip();
+       } else if (e.which === 119 && thereIsANextClip()) {
+         videoplayer[0].pause();
+         $scope.videoPlayerView = 'clipLoading';
+         $scope.$apply();
+         $scope.playNextClip();
+       }
+     });
+
+     $scope.$on('$destroy', function () {
+       $document.unbind('keypress');
+       videoplayer.unbind('timeupdate ended');
+     });
+
+    function currentClipIsOver () {
+      return $scope.currentClip && $scope.currentClip.end <= videoplayer[0].currentTime;
+    }
+
+    function thereIsANextClip () {
+      if ($scope.currentClip) {
+        var indexOfNextClip = $scope.clips.indexOf($scope.currentClip) + 1;
+        return $scope.clips.length > indexOfNextClip;
+      } else if ($scope.fullClipSelected) {
+        return $scope.clips.length > 0;
+      } else {
+        return $scope.videoPlayerView === 'noClipSelected';
+      }
+    }
+
+    function thereIsAPreviousCLip () {
+      if ($scope.fullClipSelected || $scope.videoPlayerView === 'noClipSelected') { return false; }
+      return true;
+    }
+
   }])
 
   .directive('clipPlayer', [function() {
@@ -42,58 +132,6 @@ angular.module('krossoverExercize', [])
       templateUrl: 'clipPlayerTemplate',
       scope: {
         editable: '='
-      }
-    };
-  }])
-
-  .directive('playNextClipIfFinished', ['$timeout', '$document', function ($timeout, $document) {
-    return {
-      restrict: 'A',
-      compile: function ($element, attr) {
-        return function ($scope, element) {
-
-          var videoplayer = angular.element($document[0].getElementById('video-player'));
-
-          videoplayer.on('timeupdate', function () {
-            if (currentClipIsOver() && thereIsANextCLip()) {
-              $scope.videoSource = null;
-              $scope.showNextClipButton = true;
-              $scope.$apply();
-            }
-          });
-
-          videoplayer.on('ended', function () {
-            $scope.videoSource = null;
-            if (thereIsANextCLip()) {
-              $scope.showNextClipButton = true;
-              $scope.$apply();
-            }
-          });
-
-          videoplayer.on('$destroy', function () {
-            videoplayer.unbind('timeupdate ended');
-          });
-
-          $scope.playNextClip = function () {
-            $scope.clipLoading = true;
-            $timeout(function () {
-              $scope.clipLoading= false;
-              var indexOfNextClip = $scope.clips.indexOf($scope.currentClip) + 1;
-              var nextClip = $scope.clips[indexOfNextClip];
-              $scope.select(nextClip);
-            }, 3000);
-          };
-
-          function currentClipIsOver () {
-            return $scope.currentClip && $scope.currentClip.end <= videoplayer[0].currentTime;
-          }
-
-          function thereIsANextCLip () {
-            var indexOfNextClip = $scope.clips.indexOf($scope.currentClip) + 1;
-            return $scope.clips.length > indexOfNextClip;
-          }
-
-        };
       }
     };
   }]);
